5b7e4ea739842a68549d419e4fdc093a39175db3
Alexander Andruschenko <tusur.alexander@gmail.com>
2018-07-05 17:47:24 +0700

    First styles
    Basic styles ok.  A little new layout.
    Proceed.

--- ClientApp/app/app.component.js
+++ ClientApp/app/app.component.js
@@ -13,7 +13,8 @@ let AppComponent = class AppComponent {
 AppComponent = __decorate([
     Component({
         selector: 'app-root',
-        templateUrl: 'app.component.html'
+        templateUrl: 'app.component.html',
+        styleUrls: ['app.style.css']
     })
 ], AppComponent);
 export { AppComponent };
--- ClientApp/app/app.component.ts
+++ ClientApp/app/app.component.ts
@@ -1,10 +1,12 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, ViewEncapsulation } from '@angular/core';
 import { MusicianService } from './app.service';
 import { FetchEmployeeComponent } from './fetchmusicians.component';
 
 @Component({
+    encapsulation: ViewEncapsulation.None,
     selector: 'app-root',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.style.css']
 })
 export class AppComponent {
     name = '';
